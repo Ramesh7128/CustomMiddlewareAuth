a1fa9168c3e3b13d121a8b32d2d56228d6911fda
ramesh7128 <ramesh7128@gmail.com>
2016-07-30 16:16:56 +0530

    test.py changes

--- authapp/tests.py
+++ authapp/tests.py
@@ -3,15 +3,21 @@ from django.core.urlresolvers import reverse
 from django.test import Client
 from .models import AccessToken
 from random import randint
-# from mock import Mock
+from mock import Mock
 from authapp.middleware.custom_token_validation import CustomTokenAuthentication
+from django.test.utils import setup_test_environment
+setup_test_environment()
+from django.test.client import Client
+c = Client()
 
 # Create your tests here.
 
 
 class RequestTokenTests(TestCase):
 
-
+	def setUp(self):
+		self.middleware = CustomTokenAuthentication()
+		
 	# Test to check when no token present in the request header
 	def test_notoken_present(self):
 
@@ -19,10 +25,18 @@ class RequestTokenTests(TestCase):
 		self.assertEqual(resp.status_code, 401)
 
 
+	def test_process_request_with_cart(self):
+		request = Mock(path='/')
+		request.method = 'GET'
+		request.META = {'HTTP_TOKEN': '6518907536324d17b2fff658fe545b00'}
+		response = self.middleware.process_request(request)
+		self.assertEqual(response.status_code, 200)
+
 	# Test to check if a valid token present in the request header
-	def test_invalid_token_present(self):
-		accessToken = AccessToken.objects.latest('id')
-		resp = self.client.get(reverse('productlist'), **{'HTTP_TOKEN':accessToken.token})
+	def test_valid_token_present(self):
+		# accessToken = AccessToken.objects.all().last()
+		resp = c.get(reverse('productlist'), {}, **{'TOKEN':'6518907536324d17b2fff658fe545b00', 'REMOTE_ADDR':'http://localhost:8000/'})
+		# resp = self.client.get(reverse('productlist'), **{'HTTP_TOKEN':accessToken.token})
 		self.assertEqual(resp.status_code,200) 
 
 
