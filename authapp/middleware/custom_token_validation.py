a1d0ebe410014f177c06847c27466d710e73152c
ramesh7128 <ramesh7128@gmail.com>
2016-07-30 14:41:52 +0530

    added checking conditon for access token present in header

--- authapp/middleware/custom_token_validation.py
+++ authapp/middleware/custom_token_validation.py
@@ -1,5 +1,5 @@
 from authapp import models
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # custom middleware class for token validation.
 from authapp.models import AccessToken
@@ -9,12 +9,18 @@ class CustomTokenAuthentication(object):
 	def process_request(self, request):
 
 		access_token = request.META.get('HTTP_TOKEN', '')
-		if AccessToken.objects.filter(token=access_token).exists():
-			return None
+		if access_token:
+			if AccessToken.objects.filter(token=access_token).exists():
+				return None
+			else:
+				res =  HttpResponse("Invalid token", status=401)
+				res["WWW-Authenticate"] = "Invalid Token"
+				return res
 		else:
 			res =  HttpResponse("Invalid token", status=401)
 			res["WWW-Authenticate"] = "Invalid Token"
 			return res
+			
 
 
 
