70bfb5091eafe32c8110b569915d16e825d92b94
ramesh7128 <ramesh7128@gmail.com>
2016-07-30 16:36:07 +0530

    fixed tests to validate tokens

--- authapp/tests.py
+++ authapp/tests.py
@@ -9,6 +9,10 @@ from django.test.utils import setup_test_environment
 setup_test_environment()
 from django.test.client import Client
 c = Client()
+import requests
+
+url = "https://headertokenauth.herokuapp.com/"
+
 
 # Create your tests here.
 
@@ -25,25 +29,21 @@ class RequestTokenTests(TestCase):
 		self.assertEqual(resp.status_code, 401)
 
 
-	def test_process_request_with_cart(self):
-		request = Mock(path='/')
-		request.method = 'GET'
-		request.META = {'HTTP_TOKEN': '6518907536324d17b2fff658fe545b00'}
-		response = self.middleware.process_request(request)
-		self.assertEqual(response.status_code, 200)
-
 	# Test to check if a valid token present in the request header
 	def test_valid_token_present(self):
-		# accessToken = AccessToken.objects.all().last()
-		resp = c.get(reverse('productlist'), {}, **{'TOKEN':'6518907536324d17b2fff658fe545b00', 'REMOTE_ADDR':'http://localhost:8000/'})
-		# resp = self.client.get(reverse('productlist'), **{'HTTP_TOKEN':accessToken.token})
-		self.assertEqual(resp.status_code,200) 
+		headers = {
+    	'token': "46cf9aa67fc14e74a7cb5b3e8dd74ec6",
+    	}
+		response = requests.request("GET", url, headers=headers)
+		self.assertEqual(response.status_code,200) 
 
 
 	# Test to check if an invalid token passed in request header
 	def test_invalid_token_present(self):
-		resp = self.client.get(reverse('productlist'), **{'HTTP_TOKEN':'39282983923'})
-		self.assertEqual(resp.status_code,401)
-
+		headers = {
+    	'token': "invalid_token",
+    	}
+		response = requests.request("GET", url, headers=headers)
+		self.assertEqual(response.status_code,401)
 
 
